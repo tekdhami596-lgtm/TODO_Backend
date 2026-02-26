6660792a55fbe4df859dcb745435dd47578c4265
tekdhami596--lgtm <tekdhami596@gmail.com>
2026-02-27 00:36:47 +0545

    update db

--- connection/db.js
+++ connection/db.js
@@ -1,17 +1,27 @@
 import { Sequelize } from "sequelize";
-import * as pg from "pg"; // 1. Import everything from pg
+import * as pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  dialectModule: pg, 
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false,
     },
+   
+    connectTimeout: 10000,
+  },
+  
+  pool: {
+    max: 1, 
+    min: 0,
+    idle: 0,
+    acquire: 10000,
+    evict: 1000,
   },
   logging: false,
 });
